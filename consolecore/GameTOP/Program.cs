a47eb72b13de3046105b74d12a76d5a7ed7750b6
Wylkerd <wylkerd@hotmail.com>
2020-11-28 11:43:36 -0300

    Initializing the game

--- consolecore/GameTOP/Program.cs
+++ consolecore/GameTOP/Program.cs
@@ -6,7 +6,27 @@ namespace GameTOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var jogo = new JogoMANEIRO("Messi");
+            jogo.IniciarJogo();
         }
     }
+
+    class JogoMANEIRO
+    {
+        // property
+        private readonly string _NomeJogador;
+
+        // constructor
+        public JogoMANEIRO(string nome)
+        {
+            _NomeJogador = nome;
+        }
+        public void IniciarJogo()
+        {
+            Console.Write($" {_NomeJogador} deu um passe");
+        }
+
+        
+    }
+
 }
