19954b5dc79db7170fb18dfb4fece23f21ece954
Wylkerd <wylkerd@hotmail.com>
2020-11-30 12:25:33 -0300

    Added Model and changed controllers

--- dotnetcore/ProAgil.WebAPI/Controllers/ValuesController.cs
+++ dotnetcore/ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -4,28 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Models;
 
 namespace ProAgil.WebAPI.Controllers
 {
     [ApiController]
-    [Route("site/[controller]")]
-    public class ValuesController : Controller
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get() 
+        public ActionResult<IEnumerable<Evento>> Get() 
         {
-            return Json(new            
-            {
-                msgm = "Hello world", 
-                nome = "Wylkerd Santos", 
-                idade = "20 anos"
-            });
+            return new Evento[] { 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e .NET Core",
+                    Local = "Santos",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular e sua novidades",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                }
+            };
         }
         [HttpGet("{id}")]
-        public ActionResult<string> GetAction(int id)
+        public ActionResult<Evento> Get(int id)
         {
-            return "value";
+            return new Evento[] { 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular e .NET Core",
+                    Local = "Santos",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular e sua novidades",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+                }
+            }.FirstOrDefault(x => x.EventoId == id);
         } 
     }
 }
