550aec0e88fc02438eb26cc5cceb53a90d32ef7e
wylkerd santos <wylkerd@hotmail.com>
2020-12-03 11:39:19 -0300

    Ngif

--- dotnetcore/ProAgil-App/src/app/Eventos/Eventos.component.ts
+++ dotnetcore/ProAgil-App/src/app/Eventos/Eventos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any;
+  eventos: any = [];
 
   constructor(private http: HttpClient) { }
 
