19ea10c439719d11f8793a06889b339d346ef47d
wylkerd santos <wylkerd@hotmail.com>
2020-12-04 22:30:34 -0300

    Added new context of entities

--- dotnetcore/ProAgil.Domain/Evento.cs
+++ dotnetcore/ProAgil.Domain/Evento.cs
@@ -10,11 +10,11 @@ namespace ProAgil.Domain
         public DateTime DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
-        public string ImagemUrl { get; set; }
+        public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public List<Lote> Lotes { get; set; }
         public List<RedeSocial> RedeSociais { get; set; }
-        public List<PalestranteEvento> PalestranteEventos { get; set; }
+        public List<PalestranteEvento> PalestrantesEventos { get; set; }
     }
 }
--- dotnetcore/ProAgil.Domain/Palestrante.cs
+++ dotnetcore/ProAgil.Domain/Palestrante.cs
@@ -7,10 +7,10 @@ namespace ProAgil.Domain
         public int Id { get; set; }
         public string Nome { get; set; }
         public string MiniCurriculo { get; set; }
-        public string ImagemUrl { get; set; }
+        public string ImagemURl { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public List<RedeSocial> RedeSociais { get; set; }
-        public List<PalestranteEvento> PalestranteEventos { get; set; }
+        public List<PalestranteEvento> PalestrantesEventos { get; set; }
     }
 }
