3f71c3b06f4e78a0aee7240d4d3584db733e21c3
wylkerd santos <wylkerd@hotmail.com>
2020-12-07 23:05:17 -0300

    Created Open Modal

--- dotnetcore/ProAgil-App/src/app/Eventos/Eventos.component.ts
+++ dotnetcore/ProAgil-App/src/app/Eventos/Eventos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Evento } from '../models/Evento';
 import { EventoService } from '../services/evento.service';
 
@@ -10,8 +11,22 @@ import { EventoService } from '../services/evento.service';
 })
 export class EventosComponent implements OnInit {
 
+  eventosFiltrados: Evento [];
+
+  eventos: Evento [];
+  imagemLargura = 50;
+  imagemMargin = 2;
+  mostrarImagem = false;
+  modalRef: BsModalRef; 
+
   ////////////// ENCAPSULAMENTO FILTRO TEMA
+  
   _filtroLista!: string;
+
+  constructor(
+      private eventoService: EventoService
+    , private modalService: BsModalService
+    ) { }
 
   get filtroLista(): string {
     return this._filtroLista;
@@ -49,17 +64,11 @@ export class EventosComponent implements OnInit {
       this.filtroCapacidade ? this.filtrarEventosByCapacidade(this.filtroCapacidade) : this.eventos; 
   }
 
-  ///////////////////////////
+  ///////////////////////////  
 
-  eventosFiltrados: Evento [];
-
-  eventos: Evento [];
-  imagemLargura = 50;
-  imagemMargin = 2;
-  mostrarImagem = false;
-  
-
-  constructor(private eventoService: EventoService) { }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
   ngOnInit() {
     this.getEventos();
