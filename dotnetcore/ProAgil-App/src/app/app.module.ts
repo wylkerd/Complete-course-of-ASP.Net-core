3b12facc8ca68d3473994fa069b8b955a624090e
wylkerd santos <wylkerd@hotmail.com>
2020-12-07 22:49:51 -0300

    Buttons and ToolsTips

--- dotnetcore/ProAgil-App/src/app/app.module.ts
+++ dotnetcore/ProAgil-App/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventosComponent } from './Eventos/Eventos.component';
-import { NavComponent } from './nav/nav.component';
-import { DateTimeFormatPipePipe } from './helps/DateTimeFormatPipe.pipe';
-import { EventoService } from './services/evento.service';
-
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { EventoService } from './services/evento.service';
+
+import { AppComponent } from './app.component';
+import { EventosComponent } from './Eventos/Eventos.component';
+import { NavComponent } from './nav/nav.component';
+
+import { DateTimeFormatPipePipe } from './helps/DateTimeFormatPipe.pipe';
+
 
 @NgModule({
   declarations: [		
