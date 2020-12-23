fb87e9bd648160106d1bf2cdb44b335f0fe3394d
Wylkerd <wylkerd@hotmail.com>
2020-12-23 12:19:06 -0300

    Configured many to many

--- dotnetcore/ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
+++ dotnetcore/ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using ProAgil.Domain;
 using ProAgil.WebAPI.Dtos;
@@ -8,9 +9,15 @@ namespace ProAgil.WebAPI.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<Evento, EventoDto>();
+            CreateMap<Evento, EventoDto>()
+                .ForMember(destinatario => destinatario.Palestrantes, opt => {
+                    opt.MapFrom(src => src.PalestrantesEventos.Select(x => x.Palestrante).ToList());
+                });
+            CreateMap<Palestrante, PalestranteDto>()
+                .ForMember(dest => dest.Eventos, opt => {
+                    opt.MapFrom(src => src.PalestrantesEventos.Select(x => x.Evento).ToList());
+                });
             CreateMap<Lote, LoteDto>();
-            CreateMap<Palestrante, PalestranteDto>();
             CreateMap<RedeSocial, RedeSocialDto>();
         }
     }
