4b49fe7c7155d3025c0383b54c6cdab3c352d967
Wylkerd <wylkerd@hotmail.com>
2020-12-28 15:11:23 -0300

    Created controller UserAuth

---
+++ dotnetcore/ProAgil.WebAPI/Controllers/UserAuthController.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ProAgil.Domain.Identity;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class UserAuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+
+        public UserAuthController(  IConfiguration config,
+                                    UserManager<User> userManager,
+                                    SignInManager<User> signInManager,
+                                    IMapper mapper)
+        {
+            _config = config;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("{GetUser}")]
+        public async Task<IActionResult> GetUser()
+        {
+            return Ok(new User());
+        }
+    }
+}
