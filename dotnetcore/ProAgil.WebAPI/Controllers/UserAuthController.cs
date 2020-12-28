4a75c23c4d3d761bfeb265517a95794aa9840dd2
Wylkerd <wylkerd@hotmail.com>
2020-12-28 15:35:09 -0300

    Register method

--- dotnetcore/ProAgil.WebAPI/Controllers/UserAuthController.cs
+++ dotnetcore/ProAgil.WebAPI/Controllers/UserAuthController.cs
@@ -1,9 +1,12 @@
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using ProAgil.Domain.Identity;
+using ProAgil.WebAPI.Dtos;
 
 namespace ProAgil.WebAPI.Controllers
 {
@@ -29,23 +32,34 @@ namespace ProAgil.WebAPI.Controllers
 
         [HttpGet]
         [Route("{GetUser}")]
-        public async Task<IActionResult> GetUser()
+        public async Task<IActionResult> GetUser(UserDto userDto)
         {
-            return Ok(new User());
+            return Ok(userDto);
         }
 
         [HttpPost]
         [Route("{Register}")]
-        public async Task<IActionResult> Register(UserDto user)
+        [AllowAnonymous] // para criar novo users não obrigando a autenticação. Permissão para ser anonimo
+        public async Task<IActionResult> Register(UserDto userDto)
         {
             try
             {
-                
+                var user = _mapper.Map<User>(userDto);
+
+                var result= await _userManager.CreateAsync(user, userDto.Password);
+
+                var userToReturn = _mapper.Map<UserDto>(user);
+
+                if (result.Succeeded)
+                {
+                    return Created("GetUser", userToReturn);
+                }
+
+                return BadRequest(result.Errors);
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
-                
-                throw;
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Banco de Dados falhou {ex.Message}");
             }
         }
     }
