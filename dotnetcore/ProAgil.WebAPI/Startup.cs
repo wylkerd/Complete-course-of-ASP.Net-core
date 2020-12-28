05bc5b1f7fc1dfb3226f720114840ba10887a445
Wylkerd <wylkerd@hotmail.com>
2020-12-28 14:12:58 -0300

    Configuration on Startup

--- dotnetcore/ProAgil.WebAPI/Startup.cs
+++ dotnetcore/ProAgil.WebAPI/Startup.cs
@@ -1,13 +1,19 @@
 using System.IO;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProAgil.Domain.Identity;
 using ProAgil.Repository;
 
 namespace ProAgil.WebAPI
@@ -27,12 +33,42 @@ namespace ProAgil.WebAPI
             services.AddDbContext<ProAgilContext>( 
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
+
+            IdentityBuilder builder = services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireDigit = false ;
+                options.Password.RequireNonAlphanumeric = false ;
+                options.Password.RequireLowercase = false ;
+                options.Password.RequireUppercase = false ;
+                options.Password.RequiredLength = 4 ;
+            });
+
+            builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
+            builder.AddEntityFrameworkStores<ProAgilContext>();
+            builder.AddRoleValidator<RoleValidator<Role>>();
+            builder.AddRoleManager<RoleManager<Role>>();
+            builder.AddSignInManager<SignInManager<User>>();
+
             // toda vez que chamar IProAgilRepository será retornado o repositorio ProAgilRepository
+
+            services.AddMvc( options => 
+                {
+                    var policy = new AuthorizationPolicyBuilder()
+                        .RequireAuthenticatedUser()
+                        .Build();
+                    options.Filters.Add(new AuthorizeFilter(policy));
+                })
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
             services.AddScoped<IProAgilRepository, ProAgilRepository>();
-
             services.AddAutoMapper(typeof(Startup));
-
-            services.AddControllers();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    // Use the default property (Pascal) casing
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
         }
 
