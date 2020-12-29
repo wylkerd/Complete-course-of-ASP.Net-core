0705988c9f6d7eb8ba5e2eb9b227988e3e7fe3db
Wylkerd <wylkerd@hotmail.com>
2020-12-28 23:19:02 -0300

    Display to register user

--- dotnetcore/ProAgil-App/src/app/User/Registration/Registration.component.ts
+++ dotnetcore/ProAgil-App/src/app/User/Registration/Registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-Registration',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(
+    public fb: FormBuilder,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.Validation();
+  }
+
+  Validation() {
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      userName: ['', Validators.required],
+      passwords: this.fb.group({
+        password: ['', Validators.required, Validators.minLength(4)],
+        confirmPassword: ['', Validators.required]
+      })
+    });
+  }
+
+  cadastrarUsuario() {
+    console.log('Cadastrar Usuário');
   }
 
 }
